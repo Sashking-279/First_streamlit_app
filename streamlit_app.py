d243f35c408eca1710f1e826295c8f030f57cd92
Sashanth <113826069+Sashking-279@users.noreply.github.com>
2023-09-06 15:01:06 +0530

    Update streamlit_app.py
    Added Smoothies header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('New Healthy Menu')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale,soinach and rocket smoothiee')
-streamlit.text('Hard boiled egg and French toast')
+streamlit.text(' 🥣 Omega 3 and Blueberry oatmeal')
+streamlit.text(' 🥗 Kale,soinach and rocket smoothiee')
+streamlit.text(' 🐔 Hard boiled egg and French toast')
+streamlit.text(' 🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
